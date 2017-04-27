8427c05bf3013926f0873334f1fbe8dc5e05b25c
Zhenyu Liang <zhenyu.liang@intel.com>
2017-04-28 05:50:18 +0800

    Improve yunos.mk and clean unused files

--- woogeen/yunos.mk
+++ woogeen/yunos.mk
@@ -1,30 +1,31 @@
-my-dir ?= .
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libwoogeen
 
 ninja-query = $(filter-out obj.host/%, $(shell ninja -C $(NINJA_OUTDIR) -t query $(1)))
-ninja-query-archive = $(addprefix $(NINJA_OUTDIR)/, $(filter %.a, $(call ninja-query, $(1))))
-ninja-query-object = $(addprefix $(NINJA_OUTDIR)/, $(filter %.o, $(call ninja-query, $(1))))
-NINJA_OUTDIR = $(LOCAL_PATH)/src/out/Release
-NINJA_TARGETS = woogeen_sdk_base woogeen_sdk_conf #woogeen_sdk_p2p
+ninja-query-archive = $(filter %.a, $(call ninja-query, $(1)))
+ninja-query-object = $(filter %.o, $(call ninja-query, $(1)))
+NINJA_OUTDIR  := $(LOCAL_PATH)/src/out/Release
+NINJA_TARGETS := woogeen_sdk_base woogeen_sdk_conf #woogeen_sdk_p2p
+NINJA_TARGETS := $(foreach t, $(NINJA_TARGETS), $(call ninja-query-archive,$(t)))
 
-LOCAL_SRC_FILES := $(foreach t, $(NINJA_TARGETS), $(call ninja-query-archive,$(t)))
-ARCHIVE_FILES   := $(filter-out $(LOCAL_SRC_FILES), $(call ninja-query-archive,all))
-LOCAL_SRC_FILES := $(foreach a, $(LOCAL_SRC_FILES), $(shell $(AR) -t $(a)))
+LOCAL_SRC_OBJECTS := $(foreach t, $(NINJA_TARGETS), $(call ninja-query-object,$(t)))
+LOCAL_SRC_OBJECTS := $(addprefix $(NINJA_OUTDIR)/, $(LOCAL_SRC_OBJECTS))
+
+LOCAL_SRC_ARCHIVES := $(filter-out $(NINJA_TARGETS), $(call ninja-query-archive,all))
+LOCAL_SRC_ARCHIVES := $(addprefix $(NINJA_OUTDIR)/, $(LOCAL_SRC_ARCHIVES))
+LOCAL_ADDITIONAL_DEPENDENCIES += $(LOCAL_SRC_ARCHIVES)
 
 LOCAL_LDFLAGS += \
-    -Wl,--start-group $(ARCHIVE_FILES) -Wl,--end-group \
+    -Wl,--start-group $(LOCAL_SRC_ARCHIVES) -Wl,--end-group \
     -Wl,--version-script=$(LOCAL_PATH)/libwoogeen.map \
     -L$(LOCAL_PATH)/prebuilt -lsioclient -lboost_system \
     -lssl -lcrypto -lexpat -lstdc++ -lm -lpthread -ldl
 
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_HOOK_CLEAN_CMDS := ninja -C $(NINJA_OUTDIR) -t clean
 
-ifeq ($(BUILD_SHARED_LIBRARY),)
-$(LOCAL_MODULE).so: $(LOCAL_SRC_FILES) $(ARCHIVE_FILES) $(MAKEFILE_LIST)
-	$(CC) -shared -m32 -Wl,--no-undefined -o $@ $(LOCAL_SRC_FILES) $(LOCAL_LDFLAGS)
-else
-include $(LOCAL_PATH)/sample/yunos.mk
-endif
+$(LOCAL_SRC_OBJECTS) $(LOCAL_SRC_ARCHIVES):
+	ninja -C $(NINJA_OUTDIR) $(subst $(NINJA_OUTDIR)/,,$@)
+
+include $(BUILD_SHARED_LIBRARY)
