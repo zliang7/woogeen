5e09407efcad8da5771221a62ce5787c936b16f3
Zhenyu Liang <zhenyu.liang@intel.com>
2017-06-03 05:36:44 +0800

    Fix "text file busy" build error

--- woogeen/yunos.mk
+++ woogeen/yunos.mk
@@ -3,10 +3,11 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libwoogeen
 
-ninja-query = $(filter-out obj.host/%, $(shell ninja -C $(NINJA_OUTDIR) -t query $(1)))
+ninja = ninja -C $(NINJA_OUTDIR)
+ninja-query = $(filter-out obj.host/%, $(shell $(ninja) -t query $(1)))
 ninja-query-archive = $(filter %.a, $(call ninja-query, $(1)))
 ninja-query-object = $(filter %.o, $(call ninja-query, $(1)))
-NINJA_OUTDIR  := $(LOCAL_PATH)/src/out/Release
+NINJA_OUTDIR  := $(LOCAL_PATH)/src/out/Debug
 NINJA_TARGETS := woogeen_sdk_base woogeen_sdk_conf #woogeen_sdk_p2p
 NINJA_TARGETS := $(foreach t, $(NINJA_TARGETS), $(call ninja-query-archive,$(t)))
 
@@ -21,11 +22,13 @@ LOCAL_LDFLAGS += \
     -Wl,--start-group $(LOCAL_SRC_ARCHIVES) -Wl,--end-group \
     -Wl,--version-script=$(LOCAL_PATH)/libwoogeen.map \
     -L$(LOCAL_PATH)/prebuilt -lsioclient -lboost_system \
-    -lssl -lcrypto -lexpat -lstdc++ -lm -lpthread -ldl
+    -lssl -lcrypto -lstdc++ -lm -lpthread -ldl
 
-LOCAL_HOOK_CLEAN_CMDS := ninja -C $(NINJA_OUTDIR) -t clean
+LOCAL_HOOK_CLEAN_CMDS := $(ninja) -t clean
 
-$(LOCAL_SRC_OBJECTS) $(LOCAL_SRC_ARCHIVES):
-	ninja -C $(NINJA_OUTDIR) $(subst $(NINJA_OUTDIR)/,,$@)
+$(LOCAL_SRC_OBJECTS) $(LOCAL_SRC_ARCHIVES): $(NINJA_OUTDIR)/yasm
+	$(ninja) $(subst $(NINJA_OUTDIR)/,,$@)
+$(NINJA_OUTDIR)/yasm:
+	$(ninja) yasm
 
 include $(BUILD_SHARED_LIBRARY)
